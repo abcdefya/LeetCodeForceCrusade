01abb59ca65018cf3e7b9de4b58bc478b9c1fa2c
abcdefya <anh.dothe47@gmail.com>
2025-09-05 00:39:42 +0700

    missing number

---
+++ LeetCode/268. Missing Number/Solution.py
@@ -0,0 +1,8 @@
+from typing import List
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        sum = n * (n + 1) // 2
+        for num in nums:
+            sum -= num
+        return sum
